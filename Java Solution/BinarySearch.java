3c4d09a9036e2d7dc18145c04fb7d3bff10d2872
Gevorg V <109519914+ibmhcr34@users.noreply.github.com>
2025-10-31 10:57:42 +0300

    Create BinarySearch.java

---
+++ Java Solution/BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch {
+    public static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 5, 9};
+        int result = search(nums, 5);
+        System.out.println(result);
+    }
+}
