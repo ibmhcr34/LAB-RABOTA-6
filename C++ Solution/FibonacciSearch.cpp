d329f0c9797eae2737e5f1e27b9b2e8db92dff0b
Gevorg V <109519914+ibmhcr34@users.noreply.github.com>
2025-10-31 11:03:38 +0300

    Update FibonacciSearch.cpp

--- C++ Solution/FibonacciSearch.cpp
+++ C++ Solution/FibonacciSearch.cpp
@@ -1,31 +1,42 @@
-class FibonacciSearch:
-    def search(self, nums, key):
-        n = len(nums)
-        a = 0
-        b = 1
-        c = a + b
-        while c < n:
-            a = b
-            b = c
-            c = a + b
-        index = -1
-        while c > 1:
-            pos = min(index + a, n - 1)
-            if nums[pos] < key:
-                c = b
-                b = a
-                a = c - b
-                index = pos
-            elif nums[pos] > key:
-                c = a
-                b = b - a
-                a = c - b
-            else:
-                return pos
-        if b == 1 and index + 1 < n and nums[index + 1] == key:
-            return index + 1
-        return -1
+#include <iostream>
+using namespace std;
 
-nums = [1, 3, 5, 8, 13, 21, 34]
-s = FibonacciSearch()
-print(s.search(nums, 13))
+class FibonacciSearch {
+public:
+    int search(int nums[], int n, int key) {
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+        while (c < n) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        int index = -1;
+        while (c > 1) {
+            int pos = min(index + a, n - 1);
+            if (nums[pos] < key) {
+                c = b;
+                b = a;
+                a = c - b;
+                index = pos;
+            } else if (nums[pos] > key) {
+                c = a;
+                b = b - a;
+                a = c - b;
+            } else {
+                return pos;
+            }
+        }
+        if (b == 1 && index + 1 < n && nums[index + 1] == key) return index + 1;
+        return -1;
+    }
+};
+
+int main() {
+    int nums[] = {1, 3, 5, 8, 13, 21, 34};
+    int n = 7;
+    FibonacciSearch s;
+    cout << s.search(nums, n, 13);
+    return 0;
+}
